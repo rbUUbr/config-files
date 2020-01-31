55e0c67268a3a8592a99ba0d4a7f33b176fe275f
Kirill Tatchihin <kirabsuir@gmail.com>
2020-01-31 17:22:07 +0300

    Add fix for shell script

--- lefthook/golang/.lefthook/pre-push/coverage.sh
+++ lefthook/golang/.lefthook/pre-push/coverage.sh
@@ -2,11 +2,11 @@
 
 echo "Run tests"
 
-$MINIMAL_COVERAGE=15
+MINIMAL_COVERAGE=15
 coverage=$(go test ./services/elasticsearch -cover |  awk '{print $5*1}')
 echo $coverage
 if (( $(echo "$coverage > $MINIMAL_COVERAGE" | bc -l) )); then
-	echo " coverage is valid!"
+	echo "coverage is valid!"
 	exit 0
 else
 	echo 1>&2 "You should write more tests :)"
